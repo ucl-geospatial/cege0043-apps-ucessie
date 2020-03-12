50e1f22ee047d275b979d2a7b508abbb0150c353
ucessie <ucessie@ucl.ac.uk>
2020-03-12 11:21:09 -0400

    port number detection

---
+++ js/getLocation.js
@@ -0,0 +1,5 @@
+function getLocation() {
+  alert('getting location');
+  navigator.geolocation.getCurrentPosition(getPosition); }
+function getPosition(position) {
+  document.getElementById('showLocation').innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude; }
