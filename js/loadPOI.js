517e9e293055722a6a36285a6c53e3579583bb5c
ucessie <ucessie@ucl.ac.uk>
2020-03-12 11:10:18 -0400

    fix bug of getting position and distance

--- js/loadPOI.js
+++ js/loadPOI.js
@@ -14,10 +14,18 @@ function getPOIdata() {
     // add the JSON layer onto the map - it will appear using the default icons
     // magnitude is 1.75 or less = pink; larger than is red
     poiLayer = L.geoJson(result, { pointToLayer: function (feature, latlng) {
-      if (feature.properties.mag > 1.75) { return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.place +"</b>"); }
-      else { return L.marker(latlng, {icon:testMarkerPink}).bindPopup("<b>"+feature.properties.place +"</b>");; } }, }).addTo(mymap);
+      var isParking = feature.properties.name.toLowerCase().indexOf("parking"); console.log(isParking);
+      switch (true) {
+        case (isParking > -1):
+        return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.name +"</b>");
+        break;
+
+        default:
+        return L.marker(latlng, {icon:testMarkerPink}).bindPopup("<b>"+feature.properties.name +"</b>");
+        break; }}}).addTo(mymap);
+
     // change the map zoom so that all the data is shown
     mymap.fitBounds(poiLayer.getBounds()); }
     }); // end of the inner function
     } // end of the ajax request
-    // end of the getEarthquakeData function
+    // end of the getPOI function
