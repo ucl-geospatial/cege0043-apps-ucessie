ca8ec475a45ed6c1c0c744309f521ba92fd682e2
ucessie <ucessie@ucl.ac.uk>
2020-03-31 23:40:44 -0400

    show user location once open the app

--- js/loadBasemap.js
+++ js/loadBasemap.js
@@ -3,16 +3,16 @@ var popup = L.popup();
 
 function onMapClick(e) { popup .setLatLng(e.latlng) .setContent("You clicked the map at " + e.latlng.toString()) .openOn(mymap); }
 console.log(loadLeafletMap);
-function loadLeafletMap() { mymap = L.map('content-wrapper').setView([51.505, -0.09], 13);
+function loadLeafletMap() { mymap = L.map('content-wrapper', {doubleClickZoom: false}).locate({setView: true, maxZoom: 13});
 // load the tiles
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', { maxZoom: 18, attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>', id: 'mapbox.streets' }).addTo(mymap);
 // now add the click event detector to the map
 mymap.on('click', onMapClick);
 // now call the code to add the markers
-addBasicMarkers(); }
+//addBasicMarkers(); }
 console.log(loadLeafletMap);
 //end code to add the leaflet map
-console.log(addBasicMarkers);
+//console.log(addBasicMarkers);
 function addBasicMarkers() {
   // create custom icon
   var testMarkerPink = L.AwesomeMarkers.icon({ icon: 'play', markerColor: 'pink' });
@@ -32,4 +32,4 @@ function addBasicMarkers() {
       L.geoJSON(geojsonFeature, { pointToLayer: function (feature, latlng) { return L.marker(latlng, {icon:testMarkerPink}); } }).addTo(mymap).bindPopup("<b>"+geojsonFeature.properties.name+" "+geojsonFeature.properties.popupContent+"<b>");
       // L.geoJSON(geojsonFeature).addTo(mymap).bindPopup("<b>"+geojsonFeature.properties.name+" "+geojsonFeature.properties.popupContent+"<b>");
   } // end code to add the basic markers
-console.log(addBasicMarkers);
+//sconsole.log(addBasicMarkers);
