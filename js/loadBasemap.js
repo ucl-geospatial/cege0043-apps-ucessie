a22d589e42afd0dc20979250aeeac840f03724d8
ucessie <ucessie@ucl.ac.uk>
2020-04-04 01:02:08 -0400

    add load basemap and add question function

--- js/loadBasemap.js
+++ js/loadBasemap.js
@@ -1,37 +1,53 @@
 var mymap;
 var popup = L.popup();
-var lon;
-var lat;
+var user_lat;
+var user_lon;
+var latlng;
 
-function onMapClick(e) { popup .setLatLng(e.latlng) .setContent("You clicked the map at " + e.latlng.toString()) .openOn(mymap); }
-console.log(loadLeafletMap);
-function loadLeafletMap() {
-mymap = L.map('content-wrapper').setView([51.52768, 120.131072], 13);
-// load the tiles
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', { maxZoom: 18, attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>', id: 'mapbox.streets' }).addTo(mymap);
-// now add the click event detector to the map
-mymap.on('click', onMapClick);
-//// now call the code to add the markers
-//addBasicMarkers();
+
+// get location from starting pointToLayer
+alert('getting location');
+function onMapClick(e) {
+  popup .setLatLng(e.latlng) .setContent("You clicked the map at " + e.latlng.toString()) .openOn(mymap);
+   document.getElementById('quiz_latitude').innerHTML = e.latlng.lat;
+   document.getElementById('quiz_longitude').innerHTML = e.latlng.lng;
+   console.log(e.latlng);
 }
-console.log(loadLeafletMap);
-//end code to add the leaflet map
-//console.log(addBasicMarkers);
 
-function getLocation() {
-  alert('getting location');
-  navigator.geolocation.getCurrentPosition(addBasicMarkers); }
+// track position
+function setLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+      function loadLeafletMap(location) {
+      lat = location.coords.latitude;
+      lon = location.coords.longitude;
+      latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+      // set lat lon position
+      mymap = L.map('content-wrapper').setView(latlng, 13);
+      // load the tiles
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', { maxZoom: 18, attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>', id: 'mapbox.streets' }).addTo(mymap);
+      // now add the click event detector to the map
+      mymap.on('click', onMapClick);
+      // now call the code to add the markers
+      addBasicMarkers();
+      //end code to add the leaflet map
+      var marker = L.marker(latlng).addTo(mymap);
+    });
+  }
+  else { document.getElementById('showLocation').innerHTML = "Geolocation is not supported by this browser."; }
+}
 
-//function getPosition(position) {
-  //document.getElementById('showLocation').innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude; }
+function onLocationFound(e) {
+  var radius = e.accuracy / 2;
+  L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+  L.circle(e.latlng, radius).addTo(map);
+}
 
 function addBasicMarkers(position) {
   // create custom icon
   var testMarkerPink = L.AwesomeMarkers.icon({ icon: 'play', markerColor: 'pink' });
-  var lat = position.coords.latitude
-  var lon = position.coords.longitude
   // add a point
-  //L.marker([51.5, -0.09]).addTo(mymap) .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+  L.marker([lat,lon]).addTo(mymap) .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
   // create a geoJSON feature -
   var geojsonFeature = {
     "type": "Feature",
@@ -41,9 +57,9 @@ function addBasicMarkers(position) {
     },
     "geometry": {
       "type": "Point",
-      "coordinates": [lat ,lon] } };
+      "coordinates": [lat, lon] } };
       // and add it to the map with icon
       L.geoJSON(geojsonFeature, { pointToLayer: function (feature, latlng) { return L.marker(latlng, {icon:testMarkerPink}); } }).addTo(mymap).bindPopup("<b>"+geojsonFeature.properties.name+" "+geojsonFeature.properties.popupContent+"<b>");
       // L.geoJSON(geojsonFeature).addTo(mymap).bindPopup("<b>"+geojsonFeature.properties.name+" "+geojsonFeature.properties.popupContent+"<b>");
   } // end code to add the basic markers
-//sconsole.log(addBasicMarkers);
+console.log(addBasicMarkers);
---
+++ js/upload_question.js
@@ -0,0 +1,62 @@
+function startDataUpload() { alert ("start data upload");
+
+  // get name/string value
+  var question_title = document.getElementById("question_title").value;
+  var question_text = document.getElementById("question_text").value;
+  var choice1 = document.getElementById("choice1").value;
+  var choice2 = document.getElementById("choice2").value;
+  var choice3 = document.getElementById("choice3").value;
+  var choice4 = document.getElementById("choice4").value;
+  var correct_answer = document.getElementById("correct_answer").value;
+  alert(question_title +  " " + question_text + " " + correct_answer);
+  // a var that hold all detail together, then can parse data in app
+  var postString = "question_title="+question_title +"&question_text="+question_text+"&choice1="+choice1+"&choice2="+choice2+"&choice3="+choice3+"&choice4="+choice4+"&correct_answer="+correct_answer;
+  alert(postString);
+
+  // now get the geometry values
+  var latitude = document.getElementById("quiz_latitude").value;
+  var longitude = document.getElementById("quiz_longitude").value;
+  postString = postString + "&latitude=" + latitude + "&longitude=" + longitude;
+  alert(postString);
+
+  // function should write here to check if user type in correct value
+  // now get the checkbox values - separate them with a | so that they can be
+  // split later on if necessary
+  var checkString = "";
+  for (var i = 1;i< 5;i++){
+    if (document.getElementById("check"+i).checked === true) { checkString = checkString + document.getElementById("check"+i).value + "||" } }
+    postString = postString + "&modulelist="+checkString;
+
+
+
+  // now get the radio button values
+  //if (document.getElementById("morning").checked) { postString=postString+"&lecturetime=morning"; }
+  //if (document.getElementById("afternoon").checked) { postString=postString+"&lecturetime=afternoon"; }
+
+  // finally add the port id
+  postString = postString +"&port_id="+httpsPortNumberAPI;
+  alert (postString); // close off the startDataUpload function
+  processData(postString); //
+}
+// create ajax request/ post - insert data to api
+function processData(postString) {
+  var serviceUrl= "https://developer.cege.ucl.ac.uk:"+ httpsPortNumberAPI+"/insertFormData"
+  $.ajax({
+    url: serviceUrl,
+    crossDomain: true,
+    type: "POST",
+    success: function(data){console.log(data); dataUploaded(data);}, data: postString }); }
+// create the code to process the response from the data server
+function dataUploaded(data) {
+// change the DIV to show the response
+document.getElementById("dataUploadResult").innerHTML = JSON.stringify(data); }
+
+
+function deleteRecord() {
+  var deleteID = document.getElementById("deleteID").value;
+  var deleteString = "id="+deleteID + "&port_id="+httpsPortNumberAPI;
+  var serviceUrl= "https://developer.cege.ucl.ac.uk:"+ httpsPortNumberAPI+"/deleteFormData";
+  $.ajax({ url: serviceUrl, crossDomain: true, type: "POST", success: function(data){
+    console.log(data); dataDeleted(data);}, data: deleteString }); }
+    function dataDeleted(data){
+      document.getElementById("dataDeleteResult").innerHTML = JSON.stringify(data); }
