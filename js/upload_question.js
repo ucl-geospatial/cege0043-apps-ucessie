6f9baed3b51590216d6727d937534d9ad1b803a3
ucessie <ucessie@ucl.ac.uk>
2020-04-05 10:07:54 -0400

    modify style

--- js/upload_question.js
+++ js/upload_question.js
@@ -6,14 +6,14 @@ function startDataUpload() { alert ("start data upload");
   // get name/string value
   var question_title = document.getElementById("question_title").value;
   var question_text = document.getElementById("question_text").value;
-  var choice1 = document.getElementById("choice1").value;
-  var choice2 = document.getElementById("choice2").value;
-  var choice3 = document.getElementById("choice3").value;
-  var choice4 = document.getElementById("choice4").value;
+  var answer_1 = document.getElementById("choice1").value;
+  var answer_2 = document.getElementById("choice2").value;
+  var answer_3 = document.getElementById("choice3").value;
+  var answer_4 = document.getElementById("choice4").value;
   var correct_answer = document.getElementById("correct_answer").value;
   alert(question_title +  " " + question_text + " " + correct_answer);
   // a var that hold all detail together, then can parse data in app
-  var postString = "question_title="+question_title +"&question_text="+question_text+"&choice1="+choice1+"&choice2="+choice2+"&choice3="+choice3+"&choice4="+choice4+"&correct_answer="+correct_answer;
+  var postString = "question_title="+question_title +"&question_text="+question_text+"&answer_1="+answer_1+"&answer_2="+answer_2+"&answer_3="+answer_3+"&answer_4="+answer_4+"&correct_answer="+correct_answer;
   alert(postString);
 
   // now get the geometry values
