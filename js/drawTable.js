4f424ba51b63ad6ec3c95e612ed8c2b7043dd095
ucessie <ucessie@ucl.ac.uk>
2020-04-14 07:20:02 -0400

    addd data base table

--- js/drawTable.js
+++ js/drawTable.js
@@ -4,6 +4,7 @@ function getHighFive() {
      url:"https://developer.cege.ucl.ac.uk:"+ httpsPortNumberAPI + "/getGeoJSON/getHighFive/"+ httpsPortNumberAPI ,
      crossDomain: true,
      success: function(result){
+       console.log("HighFive!")
        console.log(result);
        loadHighFive(result);
           }});
@@ -61,6 +62,7 @@ function getHighFive() {
       url:"https://developer.cege.ucl.ac.uk:"+ httpsPortNumberAPI + "/getGeoJSON/quizquestions/"+ httpsPortNumberAPI ,
       crossDomain: true,
       success: function(result){
+        console.log("Question Table!")
         console.log(result);
         loadQuestion(result);
            }});
@@ -69,19 +71,19 @@ function getHighFive() {
 
   // adopt https://gist.github.com/jfreels/6734025
   function loadQuestion(result){
-    rank_arr = result.properties;
+    rank_arr = result[0].features;
     console.log(typeof rank_arr);
 
       function tabulate(data, columns) {
     		var table = d3.select('#questionTable').append('table');
     		var thead = table.append('thead');
     		var	tbody = table.append('tbody');
-       console.log(data);
+       console.log(data[0].properties);
     		// append the header row
     		thead.append('tr')
     		  .selectAll('th')
     		  .data(columns)
-         .enter()
+          .enter()
     		  .append('th')
     		  .text(function (column) { return column; });
        console.log(thead);
@@ -95,17 +97,17 @@ function getHighFive() {
     		var cells = rows.selectAll('td')
     		  .data(function (row) {
     		    return columns.map(function (column) {
-    		      return {column: column, value: row[column]};
+    		      return {column: column, value: row.properties[column]};
     		    });
     		  })
     		  .enter()
     		  .append('td')
-    		    .text(function (d) { return d.value; });
+    		  .text(function (d) { return d.value; });
 
     	  return table;
     	}
 
     	// render the table(s)
-    	tabulate(rank_arr, ['id', 'question_title', 'question_text', 'answer_1', 'anwser_2', 'answer_3', 'answer_4', 'correct_answer', 'port_id']); // 9 column table
+    	tabulate(rank_arr, ['id', 'question_title', 'question_text', 'answer_1', 'answer_2', 'answer_3', 'answer_4', 'correct_answer', 'port_id']); // 9 column table
 
   }
