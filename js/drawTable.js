6f51d3debe1c5d665117371b7c925e063319ea84
ucessie <ucessie@ucl.ac.uk>
2020-04-15 01:16:58 -0400

    show the 5 most difficult question

--- js/drawTable.js
+++ js/drawTable.js
@@ -169,3 +169,62 @@ function getLastWeekTable() {
      tabulate(rank_arr, ['id', 'question_title', 'question_text', 'answer_1', 'answer_2', 'answer_3', 'answer_4', 'correct_answer', 'port_id']); // 9 column table
 
  }
+
+
+ // get a list of five most difficult question
+
+ function getDiffFiveTable() {
+   $.ajax(
+     {
+      url:"https://developer.cege.ucl.ac.uk:"+ httpsPortNumberAPI + "/getGeoJSON/getDifficult/"+ httpsPortNumberAPI ,
+      crossDomain: true,
+      success: function(result){
+        console.log("Last Week Question Table!")
+        console.log(result);
+        loadDiffFive(result);
+           }});
+    //end of the AJAX call
+  }
+  // idea came from: https://stackoverflow.com/questions/9268645/creating-a-table-linked-to-a-csv-file
+  // adopt https://gist.github.com/jfreels/6734025
+  function loadDiffFive(result){
+    rank_arr = result[0].array_to_json;
+    console.log(typeof rank_arr);
+
+      function tabulate(data, columns) {
+        var table = d3.select('#diffTable').append('table');
+        var thead = table.append('thead');
+        var	tbody = table.append('tbody');
+       console.log(data[0].properties);
+        // append the header row
+        thead.append('tr')
+          .selectAll('th')
+          .data(columns)
+          .enter()
+          .append('th')
+          .text(function (column) { return column; });
+       console.log(thead);
+        // create a row for each object in the data
+        var rows = tbody.selectAll('tr')
+          .data(data)
+          .enter()
+          .append('tr');
+       console.log(rows);
+        // create a cell in each row for each column
+        var cells = rows.selectAll('td')
+          .data(function (row) {
+            return columns.map(function (column) {
+              return {column: column, value: row[column]};
+            });
+          })
+          .enter()
+          .append('td')
+          .text(function (d) { return d.value; });
+
+        return table;
+      }
+
+      // render the table(s)
+      tabulate(rank_arr, ['id', 'question_title', 'question_text', 'answer_1', 'answer_2', 'answer_3', 'answer_4', 'correct_answer', 'port_id']); // 9 column table
+
+  }
